bb205f21975842247858faf60cbe55ac06c070ca
Crisostomo <johnrenzo.crisostomo07@gmail.com>
2024-01-31 16:52:30 +0800

    login and signup

---
+++ Pages/login.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login Form</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+
+
+
+            <div class="container d-flex justify-content-center align-items-center vh-100">
+                <div class="card col-md-10 shadow-lg">
+                
+        
+                    <div class="card-body">
+                        <div class="row">
+
+                            <div class="col-md-5">
+                                <h1>hello</h1>
+                            </div>
+
+                            <div class="col-md-7 p-4">
+
+                            <p class="text-center fs-4 fw-bold">SIGN IN</p>
+                            <form>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
+                                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                            </div>
+
+                            <div class="text-center">        
+                            <button type="button" class="btn btn-primary col-md-9 mx-auto">Submit</button>
+                            </div>
+
+                            <p class="text-body-secondary text-center">
+                            Need an account? <a href="../Pages/signup.php" class="text-reset">SIGNUP</a>.
+                            </p>
+                        
+                            </form>
+                            </div>
+
+
+
+
+                        </div>
+
+
+                    </div>
+
+
+
+                </div>
+
+
+            </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
